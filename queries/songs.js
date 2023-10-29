cff2d007fced4953a02f612bbc122682b6a11ccb
Joseph-Carter <1joseph1carter@gmail.com>
2023-10-29 14:13:34 -0400

    adds id to array in upddate query

--- queries/songs.js
+++ queries/songs.js
@@ -42,7 +42,7 @@ const deleteSong = async (id) => {
 const updateSong = async (id, song) => {
     try {
         const { name, artist, album, time, is_favorite } = song;
-        const updatedSong = await db.one("UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 RETURNING *", [name, artist, album, time, is_favorite]);
+        const updatedSong = await db.one("UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 RETURNING *", [name, artist, album, time, is_favorite, id]);
         return updatedSong
 
     } catch (error) {
